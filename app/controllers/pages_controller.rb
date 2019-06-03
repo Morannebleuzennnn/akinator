6c8760394a8d34961e55b39d95ffee75bba38269
MoranneBleuzen <moranne.bleuzen@kedgebs.com>
2019-06-03 15:14:33 +0200

    petites modifs

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,22 +1,22 @@
 class PagesController < ApplicationController
-  
+
   def home
   end
 
   def blog
 article1 = {
 	"title" => "Mon 1er article",
-	"content" => "salut salut salut salut",
-} 
+	"content" => "Voici mon premier article",
+}
 
 article2 = {
 	"title" => "Mon 2ème article",
-	"content" => "cou cou cou cou couc ot",
+	"content" => "Voici mon deuxième article",
 }
 
 article3 = {
 	"title" => "Mon 3ème article",
-	"content" => "hola hola hola ",
+	"content" => "Voici mon troisieme article",
 }
 
 @articles = [article1, article2, article3]
